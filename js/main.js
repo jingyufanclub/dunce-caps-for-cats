399d82151a6bbbf9ef5b3b1283d40d53d62275d3
deaundrapeek <jingyufanclub@gmail.com>
2017-04-09 02:32:54 -0400

    fix z-index

--- js/main.js
+++ js/main.js
@@ -3,9 +3,11 @@ window.onload = function () {
   var dunce = document.getElementById("dunce"),
       title = document.getElementsByTagName("h1"),
       rufus = document.getElementById("rufus");
-  TweenLite.set(dunce, {height: "50vh"});
-  TweenLite.set(rufus, {height: "70vh"});
 
-  Draggable.create([dunce, title, rufus]);
+  TweenLite.set(dunce, {height: "45vh", position: "relative"});
+  TweenLite.set(title, {position: "relative"});
+  TweenLite.set(rufus, {height: "70vh", position: "relative"});
+
+  Draggable.create([dunce, title, rufus])
 
 }
