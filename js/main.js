9dd1710b6151c9d673f94769ffbc9786ea4c6e69
deaundrapeek <jingyufanclub@gmail.com>
2017-04-09 02:18:20 -0400

    image size

--- js/main.js
+++ js/main.js
@@ -3,7 +3,9 @@ window.onload = function () {
   var dunce = document.getElementById("dunce"),
       title = document.getElementsByTagName("h1"),
       rufus = document.getElementById("rufus");
+  TweenLite.set(dunce, {height: "50vh"});
+  TweenLite.set(rufus, {height: "70vh"});
 
-  Draggable.create([dunce, title, rufus], {zIndexBoost:true});
+  Draggable.create([dunce, title, rufus]);
 
 }
