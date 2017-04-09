74a2555a6ae8c1d64f183bf4149a682eaf9667da
deaundrapeek <jingyufanclub@gmail.com>
2017-04-09 02:46:51 -0400

    fix initial render

--- js/main.js
+++ js/main.js
@@ -1,13 +1,9 @@
 window.onload = function () {
 
   var dunce = document.getElementById("dunce"),
-      title = document.getElementsByTagName("h1"),
-      rufus = document.getElementById("rufus");
-
-  TweenLite.set(dunce, {height: "45vh", position: "relative"});
-  TweenLite.set(title, {position: "relative"});
-  TweenLite.set(rufus, {height: "70vh", position: "relative"});
-
-  Draggable.create([dunce, title, rufus])
+      rufus = document.getElementById("rufus"),
+      title = document.getElementsByTagName("h1");
+      
+  Draggable.create([dunce, rufus, title])
 
 }
