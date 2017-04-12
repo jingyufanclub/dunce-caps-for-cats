cebba0d44aa65e3b7a5f6a6a9c6ad862ee0e02a7
deaundrapeek <jingyufanclub@gmail.com>
2017-04-12 14:32:35 -0400

    add var for ghost

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 window.onload = function() {
 
-  /* save these variables for later
-      var dunce = document.getElementById("dunce"),
+/* save these vars for later
+  var dunce = document.getElementById("dunce"),
+      ghost = document.getElementById("ghost"),
       kivicks = document.getElementById("kivicks"),
       rufus = document.getElementById("rufus"),
-      title = document.getElementById("title"); */
-
+      title = document.getElementById("title");
+*/
   Draggable.create("img");
-
 }
