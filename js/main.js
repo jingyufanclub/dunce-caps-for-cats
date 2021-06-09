919f972edc39f4f6fbdb9dc5b488b5b9508ff2a7
Jing <jingyufanclub@gmail.com>
2021-06-08 21:57:27 -0400

    fix copyright year

--- js/main.js
+++ js/main.js
@@ -1,13 +1,5 @@
 window.onload = function() {
+  document.getElementById("year").innerHTML = new Date().getFullYear()
 
-/* save these vars for later
-const dunce = document.getElementById("dunce"),
-      ghost = document.getElementById("ghost"),
-      kivicks = document.getElementById("kivicks"),
-      rufus = document.getElementById("rufus"),
-      title = document.getElementById("title");
-      wurtz = document.getElementById("wurtz");
-*/
   Draggable.create("img");
-  
 }
